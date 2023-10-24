3e75a37eef28db2763f78fcc09dab8e7c30de670
Ines <ines.frederickx@student.ehb.be>
2023-10-24 10:27:31 +0200

    fixed details like automatic scrolling

--- src/App.js
+++ src/App.js
@@ -15,23 +15,27 @@ function App() {
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room)
+      setShowChat(true);
     }
   }
   return (
     <div className="App">
-    
+    {!showChat ? (
     <div className='joinChatContainer'>
       <h3>Join chat</h3>
-      <input type="text" placeholder='John...' onChange={(event) =>
+      <input type="text" placeholder='Name' onChange={(event) =>
         {setUsername(event.target.value)}} />
       <input type="text" placeholder='Room ID' onChange={(event) =>
         {setRoom(event.target.value)}} />
       <button onClick={joinRoom} >Join A Room</button>
     </div>
+    )
+    : (
       <Chat socket={socket} username={username} room={room} />
-
+    )}
     </div>
+    
   );
-}
+};
 
 export default App;
--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ScrollToBottom from 'react-scroll-to-bottom';
 import { useState } from "react";
 import { useEffect } from "react";
 
 
 function Chat({ socket, username, room }) {
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
+
     const sendMessage = async () => {
         if (currentMessage !== "") {
             const messageData = {
@@ -16,12 +19,14 @@ function Chat({ socket, username, room }) {
             };
 
             await socket.emit("send_message", messageData)
+            setMessageList((list) => [...list, messageData]);
+            setCurrentMessage("");
         }
     };
 
     useEffect(() => {
         const messageHandler = (data) => {
-            console.log(data);
+            setMessageList((list) => [...list, data]);
         };
     
         socket.on("receive_message", messageHandler);
@@ -37,10 +42,34 @@ function Chat({ socket, username, room }) {
         <div className='chat-header'>
             <p>Live Chat</p>
         </div>
-        <div className='chat-body'></div>
+        <div className='chat-body'>
+            <ScrollToBottom className='message-container'>
+            {messageList.map((messageContent) => {
+                return <div className='message' id={username == messageContent.author ? "you" : "other"}>
+                    <div>
+                        <div className='message-content'>
+                            <p>{messageContent.message}</p>
+                        </div>
+                        <div className='message-meta'>
+                            <p id="time">{messageContent.time}</p>
+                            <p id="author">{messageContent.author}</p>
+                        </div>
+                    </div>
+                </div>
+            })}
+            </ScrollToBottom>
+        </div>
         <div className='chat-footer'>
-            <input type="text" placeholder='Hey...' onChange={(event) =>
-        {setCurrentMessage(event.target.value)}} />
+            <input
+                type="text"
+                value={currentMessage}
+                placeholder='Hey...'
+                onChange={(event) => {
+                setCurrentMessage(event.target.value);
+                }}
+                onKeyPress={(event) => {event.key == "Enter" && sendMessage();
+                }}
+                />
             <button onClick={sendMessage}>&#9658;</button>
         </div>
     </div>
